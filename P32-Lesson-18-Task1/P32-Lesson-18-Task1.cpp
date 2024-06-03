9ae36243ce476c72dc8c2d29118d43c4fc2f6bce
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-03 19:01:05 +0300

    create array of Computers

--- P32-Lesson-18-Task1/P32-Lesson-18-Task1.cpp
+++ P32-Lesson-18-Task1/P32-Lesson-18-Task1.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
+#include "Computer.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int N = 5;
+    Computer* arr = new Computer[N]{
+        {"HP 3000", 2.4, 8, false, 16500},
+        {"Apple Pro 2016", 3.2, 16, false, 66500},
+        {"Acer navy", 1.8, 4, true, 8200},
+        {"Samsung Son", 3.2, 16, true, 20500},
+        {"Asus 1200", 1.4, 4, false, 5500}
+    };
+
+
+
 }
 
 
